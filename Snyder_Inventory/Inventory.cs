b6b68e7d2997d83e962e321d0cf2ac4845805845
antba <antba@DESKTOP-SVL14FT>
2023-06-12 17:15:28 -0700

    Add project files.

---
+++ Snyder_Inventory/Inventory.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snyder_Inventory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    interface IShippable
+    {
+        decimal ShipCost { get; }
+        string Product { get; }
+    }
+
+    class Bicycle : IShippable
+    {
+        public decimal ShipCost { get; } = 9.50m;
+        public string Product { get; } = "Bicycle";
+    }
+
+    class LawnMower : IShippable
+    {
+        public decimal ShipCost { get; } = 24.00m;
+        public string Product { get; } = "Lawn Mower";
+    }
+
+    class BaseballGlove : IShippable
+    {
+        public decimal ShipCost { get; } = 3.23m;
+        public string Product { get; } = "Baseball Glove";
+    }
+
+    class Crackers : IShippable
+    {
+        public decimal ShipCost { get; } = 0.57m;
+        public string Product { get; } = "Crackers";
+    }
+
+    class Shipper
+    {
+        private List<IShippable> items;
+
+        public Shipper()
+        {
+            items = new List<IShippable>();
+        }
+
+        public void Add(IShippable item)
+        {
+            if (items.Count < 10)
+            {
+                items.Add(item);
+                Console.WriteLine($"1 {item.Product} has been added");
+            }
+            else
+            {
+                Console.WriteLine("Maximum capacity reached. Cannot add more items.");
+            }
+        }
+
+        public void ListItems()
+        {
+            Console.WriteLine("Shipment manifest:");
+            var groupedItems = items.GroupBy(item => item.Product);
+            foreach (var group in groupedItems)
+            {
+                int count = group.Count();
+                string product = group.Key + (count > 1 ? "s" : "");
+                Console.WriteLine($"{count} {product}");
+            }
+        }
+
+        public void ComputeShippingCharges()
+        {
+            decimal totalCost = items.Sum(item => item.ShipCost);
+            Console.WriteLine($"Total shipping cost for this order is {totalCost.ToString("C")}");
+            Console.WriteLine("Press any key to terminate the program");
+            Console.ReadKey();
+        }
+    }
+
+    
+
+}
---
+++ Snyder_Inventory/Program.cs
@@ -0,0 +1,70 @@
+﻿// Anthony Snyder
+// IT112 
+// NOTES: Is this the last project?
+// BEHAVIORS NOT IMPLEMENTED AND WHY: An exit option, i dont know but in the last hour ive been having a rough time remebering, and there seems to not be one?
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snyder_Inventory
+{
+    internal class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                Shipper shipper = new Shipper();
+
+                while (true)
+                {
+                    Console.WriteLine("Choose from the following options:");
+                    Console.WriteLine("1. Add a Bicycle to the shipment");
+                    Console.WriteLine("2. Add a Lawn Mower to the shipment");
+                    Console.WriteLine("3. Add a Baseball Glove to the shipment");
+                    Console.WriteLine("4. Add Crackers to the shipment");
+                    Console.WriteLine("5. List Shipment Items");
+                    Console.WriteLine("6. Compute Shipping Charges");
+
+                    int choice;
+                    if (int.TryParse(Console.ReadLine(), out choice))
+                    {
+                        switch (choice)
+                        {
+                            case 1:
+                                shipper.Add(new Bicycle());
+                                break;
+                            case 2:
+                                shipper.Add(new LawnMower());
+                                break;
+                            case 3:
+                                shipper.Add(new BaseballGlove());
+                                break;
+                            case 4:
+                                shipper.Add(new Crackers());
+                                break;
+                            case 5:
+                                shipper.ListItems();
+                                break;
+                            case 6:
+                                shipper.ComputeShippingCharges();
+                                return;
+                            default:
+                                Console.WriteLine("Invalid choice. Please try again.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice. Please try again.");
+                    }
+
+                    Console.WriteLine("Press any key to return to the menu");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+    }
+}
